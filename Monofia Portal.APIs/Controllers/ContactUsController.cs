7f61185ae9124346232634ed12a6345d3f3bda60
mazen_khattab <mazenkhtab11@gmail.com>
2025-05-08 19:12:11 +0300

    some updates

--- Monofia Portal.APIs/Controllers/ContactUsController.cs
+++ Monofia Portal.APIs/Controllers/ContactUsController.cs
@@ -16,28 +16,7 @@ namespace Monofia_Portal.APIs.Controllers
             _repository = repository;
             _emailService = emailService;
         }
-        #region comment 
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<ContactUs>>> GetAll() =>
-        //    Ok(await _repository.GetAllAsync());
-
-        //[HttpPost("send-message")] // POST: /api/ContactUs/send-message
-        //public async Task<IActionResult> SendMessage([FromBody] ContactUsRequest request)
-        //{
-        //    if (string.IsNullOrEmpty(request.Email) || string.IsNullOrEmpty(request.Description))
-        //    {
-        //        return BadRequest("Email and Description are required.");
-        //    }
-
-        //    var adminEmail = "mohamedhosnymohamedbaza2021@gmail.com"; // Replace with actual admin email
-        //    var subject = "New Contact Us Message";
-        //    var body = $"From: {request.Email}\n\nMessage:\n{request.Description}";
-
-        //    await _emailService.SendEmailAsync(adminEmail, subject, body);
-
-        //    return Ok("Message sent successfully.");
-        //}
-        #endregion
+       
 
         [HttpPost("contact-us")] // يحدد إن هذا الميثود هو endpoint من نوع POST على الرابط: /api/ContactUs/contact-us
         public async Task<IActionResult> ContactUs([FromBody] ContactUsRequest request) // بياخد البيانات المبعوتة من الفرونت (Email + Description)
@@ -46,22 +25,10 @@ namespace Monofia_Portal.APIs.Controllers
             if (string.IsNullOrWhiteSpace(request.Email) || string.IsNullOrWhiteSpace(request.Description))
                 return BadRequest("Email and Description are required.");
 
-<<<<<<< HEAD
             var adminEmail = "mazenkhtab11@gmail.com"; // Replace with actual admin email
             var subject = "New Contact Us Message";
             var body = $"From: {request.Email}\n\nMessage: {request.Description}\nRate: {request.Rate}";
-=======
-            // إيميل الأدمن اللي هنبعت له الرسالة
-            var adminEmail = "mohamedhosnymohamedbaza2021@gmail.com"; // استبدلها بإيميل الأدمن الفعلي
 
-            // عنوان الرسالة (Subject)
-            var subject = "New Contact Us Message";
-
-            // جسم الرسالة (Body) وبيحتوي على إيميل المستخدم ورسالة التواصل
-            var body = $"From: {request.Email}\n\nMessage:\n{request.Description}";
->>>>>>> 745ce6dab0971a0930a040b2ef94eb42f969d211
-
-            // استدعاء خدمة الإيميل علشان تبعت الرسالة
             await _emailService.SendEmailAsync(adminEmail, subject, body);
 
             // يرجع رد ناجح للفرونت إنه تم إرسال الرسالة
